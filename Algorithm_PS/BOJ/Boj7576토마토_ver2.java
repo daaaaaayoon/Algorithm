f57fddf5722981f3417d7dc75bef78db7f09d83a
daaaaaayoon <ekdbs628@gmail.com>
2020-09-16 14:13:58 +0900

    [ADD] Problem Solving close #30

---
+++ Algorithm_PS/BOJ/Boj7576토마토_ver2.java
@@ -0,0 +1,85 @@
+package BOJ;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+// ver2 : 큐에 들어있는 사이즈만큼 for문 돌리기 (출발로부터 같은 거리에 있는 정점들만 처리)
+public class Boj7576토마토_ver2 {
+	
+	static int day;
+	static int M, N; // 가로 -> 열의 길이, 세로-> 행의 길이
+	static int[][] box;
+	static Queue<Point> queue;
+	static int[] di = {0,0,-1,1};
+	static int[] dj = {1,-1,0,0};
+	
+	static class Point{
+		int i, j;
+		Point(int i, int j){
+			this.i = i;
+			this.j = j;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		M = sc.nextInt();
+		N = sc.nextInt();
+		
+		box = new int[N][M];
+		queue = new LinkedList<>();
+		
+		boolean done = true;
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				box[i][j] = sc.nextInt();
+				if(box[i][j]==0) done = false;
+				if(box[i][j]==1) {
+					queue.add(new Point(i, j));
+				}
+			}
+		}
+		
+		if(done) {
+			System.out.println(0);
+			return;
+		}
+		
+		bfs();
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(box[i][j]==0) {
+					System.out.println(-1);
+					return;
+				}
+			}
+		}
+		
+		System.out.println(day-1);
+	}
+	
+	static void bfs() {
+		
+		while(!queue.isEmpty()) {
+			int qSize = queue.size();
+			
+			for(int s=0; s<qSize; s++) {
+				Point n = queue.poll();
+				
+				for(int d=0; d<4; d++){
+					int ni = n.i + di[d];
+					int nj = n.j + dj[d];
+					if(ni>=0 && ni<N && nj>=0 && nj<M 
+							&& box[ni][nj]==0) {
+						box[ni][nj] = 1;
+						queue.add(new Point(ni, nj));
+					}
+				}
+			}
+			
+			day++;
+		}
+	}
+}
