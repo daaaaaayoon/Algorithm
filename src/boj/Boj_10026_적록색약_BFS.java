13ac8a7d38a430cf1f6b5148ac220bd27830dd90
daaaaaayoon <ekdbs628@gmail.com>
2021-08-23 19:04:10 +0900

    [resolve] bfs

--- src/boj/Boj_10026_적록색약_BFS.java
+++ src/boj/Boj_10026_적록색약_BFS.java
@@ -7,83 +7,87 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Boj_10026_적록색약_BFS {
-	
-	static int N, ans1, ans2; 
-	static char[][] map;
-	static boolean[][] visit;
-	static Queue<Point> queue;
-	static int[] di = {0,0,-1,1};
-	static int[] dj = {-1,1,0,0};
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		
-		map = new char[N][N];
-		queue = new LinkedList<>();
-		
-		for(int i=0; i<N; i++) {
-			map[i] = br.readLine().toCharArray();
-		}
-		
-		visit = new boolean[N][N];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(!visit[i][j]) {
-					bfs(i, j);
-					ans1++;
-				}
-			}
-		}
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(map[i][j]=='R') {
-					map[i][j] = 'G';
-				}
-			}
-		}
-		
-		visit = new boolean[N][N];
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(!visit[i][j]) {
-					bfs(i, j);
-					ans2++;
-				}
-			}
-		}
-		
-		System.out.println(ans1+" "+ans2);
-	}
-	
-	static void bfs(int i, int j) {
-		
-		visit[i][j] = true;
-		queue.add(new Point(i, j));
-		
-		while(!queue.isEmpty()) {
-			Point n = queue.poll();
-			
-			for(int d=0; d<4; d++) {
-				int ni = n.i + di[d];
-				int nj = n.j + dj[d];
-				if(ni>=0 && ni<N && nj>=0 && nj<N 
-						&& map[i][j] == map[ni][nj] && !visit[ni][nj]) {
-					queue.add(new Point(ni, nj));
-					visit[ni][nj] = true;
-				}
-			}
-		}
-		
-	}
-	
-	static class Point{
-		int i;
-		int j;
-		Point(int i, int j){
-			this.i = i;
-			this.j = j;
-		}
-	}
+
+    static int N;
+    static char[][] map;
+    static boolean[][] visit;
+    static int[][] didj = {{0,1}, {1,0}, {-1,0}, {0,-1}};
+    static Queue<Point> queue;
+
+    static class Point {
+        int i, j;
+        Point(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        visit = new boolean[N][N];
+
+        for(int i=0; i<N; i++) {
+            map[i]=br.readLine().toCharArray();
+        }
+
+        queue = new LinkedList<>();
+        for(int n=0; n<2; n++) {
+
+            int cnt = 0;
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    if(!visit[i][j]) {
+                        bfs(i, j);
+                        cnt++;
+                    }
+                }
+            }
+            sb.append(cnt).append(" ");
+
+            if(n==1) break;
+            changeMap();
+            visit = new boolean[N][N];
+
+        }
+
+        System.out.println(sb.toString());
+
+    }
+
+    private static void changeMap() {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(map[i][j]=='R') map[i][j] ='G';
+            }
+        }
+    }
+
+    private static void bfs(int i, int j) {
+
+        queue.add(new Point(i, j));
+        visit[i][j] = true;
+
+        while(!queue.isEmpty()) {
+
+            Point p = queue.poll();
+
+            for(int d=0; d<4; d++) {
+                int ni = p.i + didj[d][0];
+                int nj = p.j + didj[d][1];
+                if(ni<0 || nj<0 || ni>=N || nj>=N) continue;
+                if(!visit[ni][nj] && map[p.i][p.j]==map[ni][nj]) {
+                    queue.add(new Point(ni, nj));
+                    visit[ni][nj] = true;
+                }
+            }
+
+        }
+
+
+    }
+
 }
